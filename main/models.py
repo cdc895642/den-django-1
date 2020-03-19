fb7ef9d7a03e7626ed4c6e52e36914f870f64859
g895642@gmail.com <cdc895642@hotmail.com>
2020-03-19 13:52:47 +0200

    1)Changed description language.2)Changed html and css.3)Added default image.

--- main/models.py
+++ main/models.py
@@ -10,7 +10,7 @@ class Post(models.Model):
     title = models.CharField(max_length=120)
     text = models.TextField(blank=True,null=True)
     category=models.CharField(max_length=20,choices=CATEGORY,blank=True,null=True)
-    img = models.ImageField( upload_to='users_img/',blank=True,null=True,default='default.png')
+    img = models.ImageField( upload_to='users_img',blank=True,null=True,default='default.png')
     file=models.FileField(upload_to='users_files/',blank=True,null=True)
     #Counter
     download=models.PositiveIntegerField(default=0)
